8c50fb102972f137455c1fe5a96e69e715d1d6b5
Pasci <hausberger.pascal@gmail.com>
2019-01-23 12:17:47 +0100

    feat: App
    also reverted to React version 16.2.0, as the project is about Redux first and foremost and using Context API is a bit different

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import reducer from './reducers'
+import middleware from './middleware'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, middleware)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+/*
+// Dispatch an action to set the user
+// (since initial state is empty)
+store.dispatch({
+  type: "SET_USER",
+  user: {
+    avatar: "https://www.gravatar.com/avatar/5c3dd2d257ff0e14dbd2583485dbd44b",
+    name: "Dave",
+    followers: 1234,
+    following: 123
+  }
+});
+
+*/
