7017e7c2b8b985aeb814d674c7db66b7884b58d4
Pasci <hausberger.pascal@gmail.com>
2019-01-26 14:27:40 +0100

    feat: New Question UI
    next up, logic to disable button when input empty & then also adding in Redux store

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -13,7 +13,58 @@ class NewQuestion extends Component {
       // TODO: form, maybe with 'or' in between
       // TODO: if one of the fields is empty, then show that input in red or make button disabled!
       <div>
-        New Question
+        <br></br>
+        <h3 className='text-center'>Add a new question</h3>
+
+        <br></br>
+
+        <h4 className='text-center'>Would You Rather...</h4>
+
+        <br></br>
+
+        <form>
+          <div class="form-row">
+            <div class="form-group col">
+              <label for="inputOptionOne">First option</label>
+              <input type="text" class="form-control" id="inputOptionOne" placeholder="First option" />
+            </div>
+          </div>
+
+          <br></br>
+
+          <div className='row'>
+            <div className='col text-center' style={{ fontSize: 50 + 'px' }}>
+              or
+            </div>
+          </div>
+
+          <br></br>
+
+          <div class="form-group">
+            <label for="inputOptionTwo">Second option</label>
+            <input type="text" class="form-control" id="inputOptionTwo" placeholder="Second option" />
+          </div>
+
+          <br></br>
+
+          <div className='row'>
+            <button type="submit" class="btn btn-primary btn-lg mx-auto">Add question</button>
+          </div>
+        </form>
+
+        {/* <form>
+          <div className="row">
+            <div className="col-5">
+              <input type="text" className="form-control" placeholder="First option" />
+            </div>
+            <div className='col-2'>
+              or
+            </div>
+            <div className="col-5">
+              <input type="text" className="form-control" placeholder="Second option" />
+            </div>
+          </div>
+        </form> */}
       </div>
     )
   }
