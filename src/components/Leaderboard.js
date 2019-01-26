7080f0e24138f7791a12bdd97df95543d3e5a40d
Pasci <hausberger.pascal@gmail.com>
2019-01-26 22:29:48 +0100

    feat: working on Leaderboard UI
    working on Flexbox, getting better.

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import _ from 'lodash'
 
@@ -19,33 +19,49 @@ class Leaderboard extends Component {
       <div>
         <h3>Leaderboard</h3>
         {/* // TODO: just do it as normal div and use styling to make it look better */}
-        <ul className='list-group'>
+        {/* <ul className='list-group'> */}
+        <Fragment>
           {sortedUsers.map((user) => {
             leaderIndex++ // because ordered list doesn't work with Bootstrap
             return (
-              <li key={user.id} className={ 'list-group-item mb-1 ' + (authedUser.id === user.id ? 'border border-success' : '') }>
-                {/* <div className='row'>
-                  <div className='col bg-primary'>
-                    Index: {leaderIndex}
+              // <li key={user.id} className={ 'list-group-item mb-1 ' + (authedUser.id === user.id ? 'border border-success' : '') }>
+              //    {leaderIndex}) {user.name}: {user.questions.length + Object.keys(user.answers).length}
+              // </li>
+              <div key={user.id} className={'row mb-2 rounded border ' + (authedUser.id === user.id ? 'border-success' : 'border-light')}>
+                <div className='col bg-primary text-center p-5'>
+                  {leaderIndex}
+                </div>
+                <div className='col bg-success'>
+                  <img src={window.location.origin + user.avatarURL} alt={user.name} width="30" height="30" />
+                  {user.name}
+                </div>
+                <div className='col bg-info'>
+                  Questions: {user.questions.length}
+                </div>
+                <div className='col bg-warning'>
+                  Answers: {Object.keys(user.answers).length}
+                </div>
+                <div className='col bg-info'>
+                  <div className='row d-flex flex-column bg-warning justify-content-center' style={{ height: 100 + '%'}}>
+                    <div className='text-center bg-success '>
+                      {user.questions.length + Object.keys(user.answers).length}
+                    </div>
+                    <div className='text-center bg-danger '>
+                      Total
+                    </div>
                   </div>
-                  <div className='col bg-success'>
-                    Name: {user.name}
-                  </div>
-                  <div className='col bg-info'>
-                    Questions: {user.questions.length}
-                  </div>
-                  <div className='col bg-warning'>
-                    Answers: {Object.keys(user.answers).length}
-                  </div>
-                  <div className='col bg-warning'>
-                    Total: {user.questions.length + Object.keys(user.answers).length}
-                  </div>
-                </div> */}
-                 {leaderIndex}) {user.name}: {user.questions.length + Object.keys(user.answers).length}
-              </li>
+                  {/* <div className='row bg-primary align-self-end'>
+                    <div className='col text-center bg-danger'>
+                      Total
+                    </div>
+                  </div> */}
+                  {/* Total: {user.questions.length + Object.keys(user.answers).length} */}
+                </div>
+              </div>
             )
           })}
-        </ul>
+        {/* // </ul> */}
+      </Fragment>
       </div>
       // TODO: Leaderboard UI -> ordered list
       // TODO: sort by score
