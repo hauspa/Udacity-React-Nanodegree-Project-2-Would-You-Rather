06343c2eebdda80fa5add5749c29a313c99dae7b
Pasci <hausberger.pascal@gmail.com>
2019-01-28 14:46:19 +0100

    feat: hide Login/-out button
    don't show the Login or Logout button in the Navbar on their respective page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,10 +28,22 @@ class Navbar extends Component {
                 <div className="navbar-brand navbar-right d-flex align-items-center">
                   <span style={{ fontSize: 16 + 'px' }}>Hello, {authedUser.name}!</span>
                   <img src={window.location.origin + authedUser.avatarPath} width="34" height="34" className="d-inline-block align-top rounded mr-2 ml-2" alt="User Avatar" />
-                  <NavLink to='/logout'><button className="btn btn-outline-success" type="button">Logout</button></NavLink>
+                  {
+                    pathname === '/logout' // don't show logout button on the Logout page
+                      ? null
+                      : <NavLink to='/logout'><button className="btn btn-outline-success" type="button">Logout</button></NavLink>
+                  }
                 </div>
               )
-              : <NavLink to='/login'><button className="btn btn-outline-success" type="button">Login</button></NavLink>
+              : (
+                <div>
+                  {
+                    pathname === '/login' // don't show logout button on the Logout page
+                    ? null
+                    : <NavLink to='/login'><button className="btn btn-outline-success" type="button">Login</button></NavLink>
+                  }
+                </div>
+              )
             }
           </div>
       </nav>
