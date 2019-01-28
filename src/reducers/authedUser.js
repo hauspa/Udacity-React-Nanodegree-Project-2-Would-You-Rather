f8389b09e86a265829a15549fac9e450dd6ac7a2
Pasci <hausberger.pascal@gmail.com>
2019-01-28 16:02:42 +0100

    style: cleanup

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -3,16 +3,7 @@ import {
   LOGOUT_AUTHED_USER,
 } from '../actions/authedUser'
 
-// for TESTING, just have a user logged in already!
-// otherwise have to login at every refresh!
-// let initial = {
-//   'id': 'johndoe',
-//   'name': 'John Doe',
-//   'avatarPath': '/avatars/michaelScott.jpg'
-// }
-
 export default function authedUser(state = null, action) {
-// export default function authedUser(state = initial, action) {
   switch (action.type) {
     case LOGIN_AUTHED_USER :
       return {
