504ac487b83c8fb4aa84f9724b8d4c464abe1547
Pasci <hausberger.pascal@gmail.com>
2019-01-28 13:17:57 +0100

    feat: improved Leaderboard UI

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,67 +1,64 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Leaderboard extends Component {
 
-
-
   render() {
-
     let { authedUser, users } = this.props
 
     let sortedUsers = Object.values(users).sort((a,b) => (Object.keys(b.answers).length + b.questions.length) - (Object.keys(a.answers).length + a.questions.length))
-    console.log('Users: ', sortedUsers)
-
     let leaderIndex = 0 // because ordered list doesn't work with Bootstrap
 
     return(
       <div>
-        <h3>Leaderboard</h3>
-        {/* <ul className='list-group'> */}
-        <Fragment>
+        <br></br>
+        <br></br>
+        <h1 className='text-center'>Leaderboard</h1>
+        <br></br>
+        <div className='mx-auto' style={{ maxWidth: 1000 + 'px' }}>
           {sortedUsers.map((user) => {
             leaderIndex++ // because ordered list doesn't work with Bootstrap
             return (
-              // <li key={user.id} className={ 'list-group-item mb-1 ' + (authedUser.id === user.id ? 'border border-success' : '') }>
-              //    {leaderIndex}) {user.name}: {user.questions.length + Object.keys(user.answers).length}
-              // </li>
-              // TODO: AuthedUser highlighted by color border!
-              <div key={user.id} className={'row mb-2 rounded border ' + (authedUser.id === user.id ? 'border-success' : 'border-light')}>
-                <div className='col-1 bg-primary text-center p-5'>
-                  {leaderIndex}
+              <div key={user.id} className={'leaderboard row mb-2 rounded border ' + (authedUser.id === user.id ? 'border-success' : 'border-dark')}>
+                <div className='col-1 bg-dark'>
+                  <div className='row h-100 align-items-center'>
+                    <div className='col text-center index'>
+                      {leaderIndex}.
+                    </div>
+                  </div>
                 </div>
-                <div className='col bg-success'>
-                  <div className='row bg-warning h-100 align-items-center justify-content-center'>
+                <div className='col bg-dark'>
+                  <div className='row h-100 align-items-center justify-content-start'>
                     <img src={window.location.origin + user.avatarURL} alt={user.name} width="50" height="50" className='rounded mr-2' />
                     {user.name}
                   </div>
                 </div>
-                <div className='col bg-info'>
-                  <div className='row d-flex flex-column bg-warning h-100 text-center'>
-                    <div className='bg-success flex-fill d-flex flex-row align-items-center justify-content-center'>
+                <div className='col bg-dark'>
+                  <div className='row d-flex flex-column h-100 text-center'>
+                    <div className='number flex-fill d-flex flex-row align-items-center justify-content-center'>
                       {user.questions.length}
                     </div>
-                    <div className='bg-danger '>
+                    <div className='bg-dark pb-2'>
                       Questions
                     </div>
                   </div>
                 </div>
-                <div className='col bg-warning'>
-                  <div className='row d-flex flex-column bg-warning h-100 text-center'>
-                    <div className='bg-success flex-fill d-flex flex-row align-items-center justify-content-center'>
+                <div className='col bg-dark'>
+                  <div className='row d-flex flex-column h-100 text-center'>
+                    <div className='number flex-fill d-flex flex-row align-items-center justify-content-center'>
                       {Object.keys(user.answers).length}
                     </div>
-                    <div className='bg-danger '>
+                    <div className='bg-dark pb-2'>
                       Answers
                     </div>
                   </div>
                 </div>
-                <div className='col bg-info'>
-                  <div className='row d-flex flex-column bg-warning h-100 text-center'>
-                    <div className='bg-primary flex-fill d-flex flex-row align-items-center justify-content-center'>
+                <div className='col bg-dark'>
+                  <div className='row d-flex flex-column h-100 text-center'>
+                    <div className='number flex-fill d-flex flex-row align-items-center justify-content-center'>
                       {user.questions.length + Object.keys(user.answers).length}
                     </div>
-                    <div className='bg-danger '>
+                    <div className='bg-dark pb-2'>
                       Total
                     </div>
                   </div>
@@ -69,8 +66,7 @@ class Leaderboard extends Component {
               </div>
             )
           })}
-        {/* // </ul> */}
-      </Fragment>
+      </div>
       </div>
     )
   }
